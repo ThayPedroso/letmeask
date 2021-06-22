d6b998d6316ca289b6cabdf0dcdae6b791e3dccc
Thayla Pedroso <thayla.pedroso88@gmail.com>
2021-06-22 20:00:05 -0300

    add eslint configuration

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -9,7 +9,7 @@ function App() {
   return (
     <BrowserRouter>
       <AuthContextProvider>
-        <Route path="/" exact component={Home} />
+        <Route path="/" exact component={ Home } />
         <Route path="/rooms/new" component={NewRoom} />
       </AuthContextProvider>
     </BrowserRouter>
