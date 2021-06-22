b7a2842cbf70a0065f8b95a2d519533fe3fcbd24
Thayla Pedroso <thayla.pedroso88@gmail.com>
2021-06-21 23:00:53 -0300

    context example

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,12 +1,21 @@
-import { Button } from './components/Button'
+import { useState, createContext } from 'react';
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import { Home } from "./pages/Home";
+import { NewRoom } from "./pages/NewRoom";
+
+export const TestContext = createContext({} as any);
 
 function App() {
+  const [value, setValue] = useState('Teste')
+
   return (
-    <>
-      <Button />
-      <Button />
-      <Button />
-    </>
+    <BrowserRouter>
+      <TestContext.Provider value={{ value, setValue }}>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" component={NewRoom} />
+      </TestContext.Provider>
+    </BrowserRouter>
   );
 }
 
