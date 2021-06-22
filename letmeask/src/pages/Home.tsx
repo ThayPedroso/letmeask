d43935fe878187f5b016988ff2cb091994fceeb2
Thayla Pedroso <thayla.pedroso88@gmail.com>
2021-06-22 00:02:06 -0300

    auth context

--- letmeask/src/pages/Home.tsx
+++ letmeask/src/pages/Home.tsx
@@ -1,30 +1,25 @@
 import { useContext } from 'react'
 import { useHistory } from 'react-router-dom'
 
-import { auth, firebase } from '../services/firebase'
+import { AuthContext } from '../App'
+import { Button } from '../components/Button'
 
 import illustrationImg from '../assets/images/illustration.svg'
 import logoImg from '../assets/images/logo.svg'
 import googleIconImg from '../assets/images/google-icon.svg'
 
-import { Button } from '../components/Button'
-
-import { TestContext } from '../App'
-
 import '../styles/auth.scss'
 
 export function Home() {
     const history = useHistory();
-    const { value, setValue } = useContext(TestContext);
+    const { user, signInWithGoogle } = useContext(AuthContext)
 
-    function handleCreateRoom() {
-        const provider = new firebase.auth.GithubAuthProvider();
-        
-        auth.signInWithPopup(provider).then(result => {
-            console.log(result)
-
-            history.push('/rooms/new')
-        })
+    async function handleCreateRoom() {
+        if (!user) {
+            await signInWithGoogle();
+        }
+        console.log('autenticou')
+        history.push('/rooms/new');
     }
 
     return (
@@ -35,7 +30,6 @@ export function Home() {
                 <p>Tire as dúvidas da sua audiência em tempo-real</p>
             </aside>
             <main>
-                <h1>{value}</h1>
                 <div className="main-content">
                     <img src={logoImg} alt="Letmeask" />
                     <button onClick={handleCreateRoom} className="create-room">
